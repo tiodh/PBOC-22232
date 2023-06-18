b059cc1bf920d5c1fce8568d7d520e2b81c5a8af
SEdwin22 <133666486+SEdwin22@users.noreply.github.com>
2023-06-18 17:17:20 +0700

    Add files via upload

---
+++ 222410103030 steven create data harga wisata/222410103030 steven create data harga wisata/Form1.cs
@@ -0,0 +1,10 @@
+namespace _222410103030_steven_create_data_harga_wisata
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

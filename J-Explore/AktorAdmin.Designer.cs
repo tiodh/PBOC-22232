e21a544ec103b63d8f4c9b03e19759c4d05bb19e
adit-wr <ganding0002nd@gmail.com>
2023-06-19 02:28:05 +0700

    template aktor admin

--- J-Explore/AktorAdmin.Designer.cs
+++ J-Explore/AktorAdmin.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace J_Explore
 {
-    partial class Form1
+    partial class AkunAdmin
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,21 +23,15 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            SuspendLayout();
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1218, 639);
-            Name = "Form1";
-            Text = "Form1";
-            ResumeLayout(false);
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "AkunAdmin";
         }
 
         #endregion

543b1d917c28cb1aec28553264b704a0643db981
ItsMeRizall <yoppy.sr2@gmail.com>
2023-06-19 04:29:15 +0700

    HomePage

--- J-Explore/Form2.Designer.cs
+++ J-Explore/Form2.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             panel1 = new Panel();
             button1 = new Button();
             panel13 = new Panel();
@@ -40,10 +41,10 @@
             panel6 = new Panel();
             panel8 = new Panel();
             panel5 = new Panel();
-            label1 = new Label();
             panel4 = new Panel();
             panel2 = new Panel();
             panel3 = new Panel();
+            HomeButton = new Button();
             panel1.SuspendLayout();
             panel5.SuspendLayout();
             SuspendLayout();
@@ -63,135 +64,158 @@
             panel1.Controls.Add(panel5);
             panel1.Controls.Add(panel4);
             panel1.Location = new Point(1, 0);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(329, 749);
+            panel1.Size = new Size(376, 999);
             panel1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(0, 706);
+            button1.Location = new Point(0, 941);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(326, 40);
+            button1.Size = new Size(373, 53);
             button1.TabIndex = 14;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
             // panel13
             // 
-            panel13.Location = new Point(3, 650);
+            panel13.Location = new Point(3, 867);
+            panel13.Margin = new Padding(3, 4, 3, 4);
             panel13.Name = "panel13";
-            panel13.Size = new Size(323, 50);
+            panel13.Size = new Size(369, 67);
             panel13.TabIndex = 13;
             // 
             // panel11
             // 
-            panel11.Location = new Point(3, 538);
+            panel11.Location = new Point(3, 717);
+            panel11.Margin = new Padding(3, 4, 3, 4);
             panel11.Name = "panel11";
-            panel11.Size = new Size(323, 50);
+            panel11.Size = new Size(369, 67);
             panel11.TabIndex = 11;
             // 
             // panel14
             // 
-            panel14.Location = new Point(3, 594);
+            panel14.Location = new Point(3, 792);
+            panel14.Margin = new Padding(3, 4, 3, 4);
             panel14.Name = "panel14";
-            panel14.Size = new Size(323, 50);
+            panel14.Size = new Size(369, 67);
             panel14.TabIndex = 12;
             // 
             // panel9
             // 
-            panel9.Location = new Point(3, 426);
+            panel9.Location = new Point(3, 568);
+            panel9.Margin = new Padding(3, 4, 3, 4);
             panel9.Name = "panel9";
-            panel9.Size = new Size(323, 50);
+            panel9.Size = new Size(369, 67);
             panel9.TabIndex = 9;
             // 
             // panel12
             // 
-            panel12.Location = new Point(3, 482);
+            panel12.Location = new Point(3, 643);
+            panel12.Margin = new Padding(3, 4, 3, 4);
             panel12.Name = "panel12";
-            panel12.Size = new Size(323, 50);
+            panel12.Size = new Size(369, 67);
             panel12.TabIndex = 10;
             // 
             // panel7
             // 
-            panel7.Location = new Point(3, 314);
+            panel7.Location = new Point(3, 419);
+            panel7.Margin = new Padding(3, 4, 3, 4);
             panel7.Name = "panel7";
-            panel7.Size = new Size(323, 50);
+            panel7.Size = new Size(369, 67);
             panel7.TabIndex = 7;
             // 
             // panel10
             // 
-            panel10.Location = new Point(3, 370);
+            panel10.Location = new Point(3, 493);
+            panel10.Margin = new Padding(3, 4, 3, 4);
             panel10.Name = "panel10";
-            panel10.Size = new Size(323, 50);
+            panel10.Size = new Size(369, 67);
             panel10.TabIndex = 8;
             // 
             // panel6
             // 
-            panel6.Location = new Point(3, 202);
+            panel6.Location = new Point(3, 269);
+            panel6.Margin = new Padding(3, 4, 3, 4);
             panel6.Name = "panel6";
-            panel6.Size = new Size(323, 50);
+            panel6.Size = new Size(369, 67);
             panel6.TabIndex = 5;
             // 
             // panel8
             // 
-            panel8.Location = new Point(3, 258);
+            panel8.Location = new Point(3, 344);
+            panel8.Margin = new Padding(3, 4, 3, 4);
             panel8.Name = "panel8";
-            panel8.Size = new Size(323, 50);
+            panel8.Size = new Size(369, 67);
             panel8.TabIndex = 6;
             // 
             // panel5
             // 
-            panel5.Controls.Add(label1);
-            panel5.Location = new Point(3, 146);
+            panel5.Controls.Add(HomeButton);
+            panel5.Location = new Point(3, 195);
+            panel5.Margin = new Padding(3, 4, 3, 4);
             panel5.Name = "panel5";
-            panel5.Size = new Size(323, 50);
+            panel5.Size = new Size(369, 67);
             panel5.TabIndex = 4;
             panel5.Click += panel5_Click;
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(60, 7);
-            label1.Name = "label1";
-            label1.Size = new Size(114, 37);
-            label1.TabIndex = 0;
-            label1.Text = "Beranda";
-            // 
             // panel4
             // 
-            panel4.Location = new Point(3, 3);
+            panel4.Location = new Point(3, 4);
+            panel4.Margin = new Padding(3, 4, 3, 4);
             panel4.Name = "panel4";
-            panel4.Size = new Size(323, 137);
+            panel4.Size = new Size(369, 183);
             panel4.TabIndex = 3;
             // 
             // panel2
             // 
-            panel2.Location = new Point(336, 143);
+            panel2.Location = new Point(384, 191);
+            panel2.Margin = new Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(1034, 606);
+            panel2.Size = new Size(1182, 808);
             panel2.TabIndex = 1;
             // 
             // panel3
             // 
-            panel3.Location = new Point(336, 0);
+            panel3.Location = new Point(384, 0);
+            panel3.Margin = new Padding(3, 4, 3, 4);
             panel3.Name = "panel3";
-            panel3.Size = new Size(1034, 137);
+            panel3.Size = new Size(1182, 183);
             panel3.TabIndex = 2;
+            // 
+            // HomeButton
+            // 
+            HomeButton.BackColor = Color.FromArgb(63, 130, 109);
+            HomeButton.Dock = DockStyle.Fill;
+            HomeButton.FlatStyle = FlatStyle.Flat;
+            HomeButton.Image = (Image)resources.GetObject("HomeButton.Image");
+            HomeButton.ImageAlign = ContentAlignment.MiddleLeft;
+            HomeButton.Location = new Point(0, 0);
+            HomeButton.Name = "HomeButton";
+            HomeButton.Padding = new Padding(15, 0, 0, 0);
+            HomeButton.Size = new Size(369, 67);
+            HomeButton.TabIndex = 0;
+            HomeButton.Text = "           button2";
+            HomeButton.TextAlign = ContentAlignment.MiddleLeft;
+            HomeButton.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1370, 749);
+            ClientSize = new Size(1566, 999);
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
+            ForeColor = Color.White;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form2";
+            RightToLeft = RightToLeft.No;
             Text = "J-Explore";
             panel1.ResumeLayout(false);
             panel5.ResumeLayout(false);
-            panel5.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -212,6 +236,6 @@
         private Panel panel4;
         private Panel panel2;
         private Panel panel3;
-        private Label label1;
+        private Button HomeButton;
     }
 }

9e3abfa7d09a0df1975d86f64ccb5af6cdeaf92b
Aryanesta <aryanesta2@gmail.com>
2023-06-20 12:23:33 +0700

    ngewei fitur read kuliner di dashboard user, tapi fitur search, sama clear e sek gk fungsi

--- J-Explore/Fitur/Kuliner.Designer.cs
+++ J-Explore/Fitur/Kuliner.Designer.cs
@@ -28,9 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             textBox1 = new TextBox();
             button1 = new Button();
             dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -43,7 +49,7 @@
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(253, 41);
             textBox1.TabIndex = 0;
-            textBox1.Text = "  Cari";
+            textBox1.Text = "\r\n";
             // 
             // button1
             // 
@@ -54,24 +60,77 @@
             button1.TabIndex = 1;
             button1.Text = "Cari";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // dataGridView1
             // 
             dataGridView1.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4 });
             dataGridView1.Location = new Point(17, 125);
             dataGridView1.Margin = new Padding(4, 5, 4, 5);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(808, 448);
+            dataGridView1.Size = new Size(739, 448);
             dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            Column1.HeaderText = "Nama Tempat";
+            Column1.MinimumWidth = 12;
+            Column1.Name = "Column1";
+            Column1.Width = 158;
+            // 
+            // Column2
+            // 
+            Column2.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            Column2.HeaderText = "Deskripsi Tempat";
+            Column2.MinimumWidth = 12;
+            Column2.Name = "Column2";
+            Column2.Width = 183;
+            // 
+            // Column3
+            // 
+            Column3.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            Column3.HeaderText = "Nama Kuliner";
+            Column3.MinimumWidth = 12;
+            Column3.Name = "Column3";
+            Column3.Width = 153;
+            // 
+            // Column4
+            // 
+            Column4.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            Column4.HeaderText = "Harga Kuliner";
+            Column4.MinimumWidth = 12;
+            Column4.Name = "Column4";
+            Column4.Width = 154;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(423, 45);
+            button2.Name = "button2";
+            button2.Size = new Size(102, 41);
+            button2.TabIndex = 3;
+            button2.Text = "Clear\r\n";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Kuliner
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(838, 587);
+            Controls.Add(button2);
             Controls.Add(dataGridView1);
             Controls.Add(button1);
             Controls.Add(textBox1);
@@ -87,5 +146,10 @@
         private TextBox textBox1;
         private Button button1;
         private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private Button button2;
     }
 }
--- J-Explore/Fitur/Kuliner.cs
+++ J-Explore/Fitur/Kuliner.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using J_Explore;
 
 namespace J_Explore.Fitur
 {
     public partial class Kuliner : Form
     {
+        string connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=12345;Database=jexplore fix";
         public Kuliner()
         {
             InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = @"SELECT nt.nama_tempat, nt.deskripsi_nama_tempat, k.nama_kuliner, k.harga_kuliner
+                                    FROM nama_tempat nt
+                                    INNER JOIN kuliner k ON nt.id_nama_tempat = k.id_nama_tempat";
+                    using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string namaTempat = reader.GetString(0);
+                                string deskripsiTempat = reader.GetString(1);
+                                string namaKuliner = reader.GetString(2);
+                                int hargaKuliner = reader.GetInt32(3);
+
+                                dataGridView1.Rows.Add(namaTempat, deskripsiTempat, namaKuliner, hargaKuliner);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string keyword = textBox1.Text;
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT c.nama_tempat, c.deskripsi_nama_tempat, d.nama_kuliner, d.harga_kuliner " +
+                    "FROM nama_tempat c JOIN kuliner d ON c.id_nama_tempat = d.id_nama_tempat " +
+                    "WHERE d.nama_kuliner LIKE '%' || @keyword || '%'";
+                using (NpgsqlCommand command = new NpgsqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@keyword", keyword);
+
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        dataGridView1.Rows.Clear();
+
+                        while (reader.Read())
+                        {
+                            string namaTempat = reader.GetString(0);
+                            string deskripsiTempat = reader.GetString(1);
+                            string namaKuliner = reader.GetString(2);
+                            int hargaKuliner = reader.GetInt32(3);
+
+                            dataGridView1.Rows.Add(namaTempat, deskripsiTempat, namaKuliner, hargaKuliner);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
         }
     }
 }

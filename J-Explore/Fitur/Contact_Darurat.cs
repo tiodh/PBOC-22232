d6cc8c485506a14d9114a7787134cca1f94b4e35
BlastTea <arya.jbr.999@gmail.com>
2023-06-20 12:59:11 +0700

    delete kontak darurat

--- J-Explore/Fitur/Contact_Darurat.cs
+++ J-Explore/Fitur/Contact_Darurat.cs
@@ -1,8 +1,10 @@
-﻿using Npgsql;
+﻿using J_Explore.Utils;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,7 +15,7 @@ namespace J_Explore.Fitur
 {
     public partial class Contact_Darurat : Form
     {
-        private const string connectionString = "Server = localhost; Port = 5432; User Id= postgres; Password=Rizal020304; Database = pbo;";
+        private string connectionString = $"Server = {Global.DbHost}; Port = {Global.DbPort}; User Id= {Global.DbUsername}; Password={Global.DbPassword}; Database = {Global.DbName};";
         public Contact_Darurat()
         {
             InitializeComponent();
@@ -22,6 +24,8 @@ namespace J_Explore.Fitur
 
         public void ReadKontakDarurat()
         {
+            DataKontakDarurat.Rows.Clear();
+
             using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
             {
                 connection.Open();
@@ -41,11 +45,33 @@ namespace J_Explore.Fitur
                         string phone = reader.GetString(3);
                         string deskripsi = reader.GetString(4);
 
-                        DataKontakDarurat.Rows.Add(id,name, email, phone, deskripsi);
+                        DataKontakDarurat.Rows.Add(id, name, email, phone, deskripsi);
                     }
                 }
 
                 connection.Close();
+            }
+        }
+
+        private void OnButtonHapusClick(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                int selectedRow = DataKontakDarurat.SelectedCells[0].RowIndex;
+                int id = Convert.ToInt32(DataKontakDarurat.Rows[selectedRow].Cells[0].Value);
+
+                string sql = $"DELETE FROM layanan_darurat WHERE id_layanan_darurat = '{id}'";
+
+                int affectedRows = new NpgsqlCommand(sql, connection).ExecuteNonQuery();
+
+                if (affectedRows > 0)
+                {
+                    MessageBox.Show("Data berhasil dihapus");
+                }
+
+                ReadKontakDarurat();
             }
         }
     }

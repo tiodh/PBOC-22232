bcfe5464d8cc62d90b998a7eacdcc5b49d7ec5d2
ItsMeRizall <yoppy.sr2@gmail.com>
2023-06-20 12:42:27 +0700

    add read kontak darurat

--- J-Explore/Fitur/Contact_Darurat.Designer.cs
+++ J-Explore/Fitur/Contact_Darurat.Designer.cs
@@ -28,153 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            label2 = new Label();
-            textBox3 = new TextBox();
-            label3 = new Label();
-            textBox4 = new TextBox();
-            label4 = new Label();
-            button1 = new Button();
-            button2 = new Button();
-            label5 = new Label();
+            panel1 = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            DataKontakDarurat = new DataGridView();
+            panel2 = new Panel();
+            HapusButton = new Button();
+            id = new DataGridViewTextBoxColumn();
+            name = new DataGridViewTextBoxColumn();
+            email = new DataGridViewTextBoxColumn();
+            phone = new DataGridViewTextBoxColumn();
+            deskripsi = new DataGridViewTextBoxColumn();
+            panel1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)DataKontakDarurat).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(76, 145);
-            label1.Name = "label1";
-            label1.Size = new Size(79, 28);
-            label1.TabIndex = 0;
-            label1.Text = "Nama";
+            panel1.Controls.Add(tableLayoutPanel1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(30);
+            panel1.Size = new Size(1250, 792);
+            panel1.TabIndex = 0;
             // 
-            // textBox1
+            // tableLayoutPanel1
             // 
-            textBox1.Location = new Point(76, 176);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(532, 46);
-            textBox1.TabIndex = 1;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80.5882339F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 19.4117641F));
+            tableLayoutPanel1.Controls.Add(DataKontakDarurat, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel2, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(30, 30);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(1190, 732);
+            tableLayoutPanel1.TabIndex = 2;
             // 
-            // textBox2
+            // DataKontakDarurat
             // 
-            textBox2.Location = new Point(76, 277);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(532, 46);
-            textBox2.TabIndex = 3;
+            DataKontakDarurat.AllowUserToAddRows = false;
+            DataKontakDarurat.AllowUserToDeleteRows = false;
+            DataKontakDarurat.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DataKontakDarurat.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DataKontakDarurat.Columns.AddRange(new DataGridViewColumn[] { id, name, email, phone, deskripsi });
+            DataKontakDarurat.Dock = DockStyle.Fill;
+            DataKontakDarurat.Location = new Point(3, 3);
+            DataKontakDarurat.Name = "DataKontakDarurat";
+            DataKontakDarurat.ReadOnly = true;
+            DataKontakDarurat.RowHeadersWidth = 51;
+            DataKontakDarurat.Size = new Size(953, 726);
+            DataKontakDarurat.TabIndex = 4;
             // 
-            // label2
+            // panel2
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(76, 246);
-            label2.Name = "label2";
-            label2.Size = new Size(131, 28);
-            label2.TabIndex = 2;
-            label2.Text = "No.Telpon";
+            panel2.Controls.Add(HapusButton);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(962, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(225, 726);
+            panel2.TabIndex = 5;
             // 
-            // textBox3
+            // HapusButton
             // 
-            textBox3.Location = new Point(76, 383);
-            textBox3.Multiline = true;
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(532, 46);
-            textBox3.TabIndex = 5;
+            HapusButton.BackColor = Color.FromArgb(179, 64, 69);
+            HapusButton.Dock = DockStyle.Top;
+            HapusButton.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            HapusButton.ForeColor = Color.Black;
+            HapusButton.Location = new Point(0, 0);
+            HapusButton.Name = "HapusButton";
+            HapusButton.Size = new Size(225, 44);
+            HapusButton.TabIndex = 2;
+            HapusButton.Text = "HAPUS";
+            HapusButton.UseVisualStyleBackColor = false;
             // 
-            // label3
+            // id
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(76, 352);
-            label3.Name = "label3";
-            label3.Size = new Size(77, 28);
-            label3.TabIndex = 4;
-            label3.Text = "Email";
+            id.HeaderText = "ID";
+            id.MinimumWidth = 6;
+            id.Name = "id";
+            id.ReadOnly = true;
             // 
-            // textBox4
+            // name
             // 
-            textBox4.Location = new Point(76, 494);
-            textBox4.Multiline = true;
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(532, 46);
-            textBox4.TabIndex = 7;
+            name.HeaderText = "NAMA";
+            name.MinimumWidth = 6;
+            name.Name = "name";
+            name.ReadOnly = true;
             // 
-            // label4
+            // email
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(76, 463);
-            label4.Name = "label4";
-            label4.Size = new Size(124, 28);
-            label4.TabIndex = 6;
-            label4.Text = "Deskripsi";
+            email.HeaderText = "EMAIL";
+            email.MinimumWidth = 6;
+            email.Name = "email";
+            email.ReadOnly = true;
             // 
-            // button1
+            // phone
             // 
-            button1.Location = new Point(76, 684);
-            button1.Name = "button1";
-            button1.Size = new Size(263, 60);
-            button1.TabIndex = 8;
-            button1.Text = "Simpan";
-            button1.UseVisualStyleBackColor = true;
+            phone.HeaderText = "NO HP";
+            phone.MinimumWidth = 6;
+            phone.Name = "phone";
+            phone.ReadOnly = true;
             // 
-            // button2
+            // deskripsi
             // 
-            button2.Location = new Point(345, 684);
-            button2.Name = "button2";
-            button2.Size = new Size(263, 60);
-            button2.TabIndex = 9;
-            button2.Text = "Update";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Arial Rounded MT Bold", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(77, 34);
-            label5.Name = "label5";
-            label5.Size = new Size(123, 32);
-            label5.TabIndex = 10;
-            label5.Text = "Contact";
+            deskripsi.HeaderText = "DESKRIPSI";
+            deskripsi.MinimumWidth = 6;
+            deskripsi.Name = "deskripsi";
+            deskripsi.ReadOnly = true;
             // 
             // Contact_Darurat
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1562, 990);
-            Controls.Add(label5);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(textBox4);
-            Controls.Add(label4);
-            Controls.Add(textBox3);
-            Controls.Add(label3);
-            Controls.Add(textBox2);
-            Controls.Add(label2);
-            Controls.Add(textBox1);
-            Controls.Add(label1);
+            ClientSize = new Size(1250, 792);
+            Controls.Add(panel1);
+            Margin = new Padding(2);
             Name = "Contact_Darurat";
             Text = "Contact_Darurat";
+            panel1.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)DataKontakDarurat).EndInit();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Label label2;
-        private TextBox textBox3;
-        private Label label3;
-        private TextBox textBox4;
-        private Label label4;
-        private Button button1;
-        private Button button2;
-        private Label label5;
+        private Panel panel1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private DataGridView DataKontakDarurat;
+        private Panel panel2;
+        private Button HapusButton;
+        private DataGridViewTextBoxColumn id;
+        private DataGridViewTextBoxColumn name;
+        private DataGridViewTextBoxColumn email;
+        private DataGridViewTextBoxColumn phone;
+        private DataGridViewTextBoxColumn deskripsi;
     }
 }
--- J-Explore/Fitur/Contact_Darurat.cs
+++ J-Explore/Fitur/Contact_Darurat.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,40 @@ namespace J_Explore.Fitur
 {
     public partial class Contact_Darurat : Form
     {
+        private const string connectionString = "Server = localhost; Port = 5432; User Id= postgres; Password=Rizal020304; Database = pbo;";
         public Contact_Darurat()
         {
             InitializeComponent();
+            ReadKontakDarurat();
+        }
+
+        public void ReadKontakDarurat()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "Select * from layanan_darurat";
+
+                using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
+                {
+                    command.CommandText = sql;
+                    NpgsqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        int id = Convert.ToInt32(reader[0]);
+                        string name = reader.GetString(1);
+                        string email = reader.GetString(2);
+                        string phone = reader.GetString(3);
+                        string deskripsi = reader.GetString(4);
+
+                        DataKontakDarurat.Rows.Add(id,name, email, phone, deskripsi);
+                    }
+                }
+
+                connection.Close();
+            }
         }
     }
 }
